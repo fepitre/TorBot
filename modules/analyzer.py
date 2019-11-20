9290a3446c526c233598d6a6dc7fe4ca561ee15a
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2019-11-21 00:18:39 +0100

    WIP

--- modules/analyzer.py
+++ modules/analyzer.py
@@ -18,51 +18,6 @@ default_style.show_leaf_name = False
 default_style.layout_fn = default_layout
 
 
-class LinkTree:
-    """
-    This is a class that represents a tree of links within TorBot. This can
-    be used to build a tree, examine the number of nodes, check if a node
-    exists within a tree, displaying the tree, and downloading the tree. It
-    will be expanded in the future to meet further needs.
-
-    Attributes:
-        root (LinkNode): root node
-        stop_depth (int): Depth of which to stop searching for links
-    """
-    def __init__(self, root_node, stop_depth=1):
-        self._tree = build_tree(root_node, stop=stop_depth)
-
-    def __len__(self):
-        return len(self._tree)
-
-    def __contains__(self, link):
-        return self._tree.search_nodes(name=link)
-
-    @property
-    def children(self):
-        return self._tree.get_children()
-
-    def save(self, file_name, tree_style=default_style):
-        """
-        Saves LinkTree to file with given file_name
-        Current file types supported are .png, .pdf, .svg
-
-        Args:
-            file_name (str): Name of file being saved to
-            tree_style (TreeStyle): Styling of downloaded tree
-        """
-        self._tree.render(file_name, tree_style)
-
-    def show(self, tree_style=default_style):
-        """
-        Displays image of LinkTree
-
-        Args:
-            tree_style (TreeStyle): Styling of downloaded tree
-        """
-        self._tree.show(tree_style)
-
-
 def build_tree(link, stop=1, rec=0):
     """
     Builds link tree by traversing through children nodes.
@@ -96,3 +51,54 @@ def build_tree(link, stop=1, rec=0):
             tree.add_child(Tree(name=node.name))
 
     return tree
+
+
+class LinkTree:
+    """
+    This is a class that represents a tree of links within TorBot. This can
+    be used to build a tree, examine the number of nodes, check if a node
+    exists within a tree, displaying the tree, and downloading the tree. It
+    will be expanded in the future to meet further needs.
+
+    Attributes:
+        root (LinkNode): root node
+        stop_depth (int): Depth of which to stop searching for links
+    """
+
+    def __init__(self, root_node, stop_depth=1):
+        self._root_node = root_node
+        self._tree = build_tree(root_node, stop=stop_depth)
+
+    def __len__(self):
+        return len(self._tree)
+
+    def __contains__(self, link):
+        return self._tree.search_nodes(name=link)
+
+    @property
+    def root_node(self):
+        return self._root_node
+
+    @property
+    def children(self):
+        return self._tree.get_children()
+
+    def save(self, file_name, tree_style=default_style):
+        """
+        Saves LinkTree to file with given file_name
+        Current file types supported are .png, .pdf, .svg
+
+        Args:
+            file_name (str): Name of file being saved to
+            tree_style (TreeStyle): Styling of downloaded tree
+        """
+        self._tree.render(file_name, tree_style)
+
+    def show(self, tree_style=default_style):
+        """
+        Displays image of LinkTree
+
+        Args:
+            tree_style (TreeStyle): Styling of downloaded tree
+        """
+        self._tree.show(tree_style)
